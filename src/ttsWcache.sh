4ac4c73f371e6b03690ff170298e34e80f0aa99d
Andreas Dominik <andreas.dominik@mni.thm.de>
2019-05-05 12:02:33 +0200

    generation of alnguage string made safer

--- src/ttsWcache.sh
+++ src/ttsWcache.sh
@@ -24,6 +24,7 @@ case $LANGUAGE in
         COUNTRY="GB"
         ;;
     *)
+        LANGUAGE="$(echo ${LANGUAGE:0:2} | tr A-Z a-z)"
         COUNTRY="$(echo $LANGUAGE | tr a-z A-Z)"
         ;;
 esac
